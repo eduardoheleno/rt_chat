47ac15b787855b032490955fa99f0b40c4e0b04f
Eduardo Gomes Heleno <eduardo.gheleno@gmail.com>
2024-04-03 01:06:02 -0300

    small changes on rust websocket

--- websocket_server/src/main.rs
+++ websocket_server/src/main.rs
@@ -4,28 +4,33 @@ use std::{
     sync::{Arc, Mutex}
 };
 
-use futures_channel::mpsc::{unbounded, UnboundedSender};
-use futures_util::{future, pin_mut, stream::TryStreamExt, StreamExt};
+use futures_channel::mpsc::{
+    unbounded, 
+    UnboundedSender
+};
+use futures_util::{
+    future,
+    pin_mut,
+    stream::TryStreamExt,
+    StreamExt
+};
 
 use tokio::net::{TcpListener, TcpStream};
-use tokio_tungstenite::tungstenite::{protocol::Message, handshake::server::Request, http::HeaderValue};
-
-use mysql_async::{prelude::*, Error, Params};
+use tokio_tungstenite::tungstenite::{
+    protocol::Message,
+    handshake::server::Request,
+    http::HeaderValue
+};
 
 type Tx = UnboundedSender<Message>;
 type PeerMap = Arc<Mutex<HashMap<String, Tx>>>;
-
-#[derive(Debug, PartialEq, Eq, Clone)]
-struct User {
-    user_nick: String,
-}
 
 async fn handle_connection(peer_map: PeerMap, raw_stream: TcpStream, addr: SocketAddr) {
     println!("Incoming TCP connection from: {}", addr);
 
     let mut client_header: Option<HeaderValue> = Default::default();
     let ws_stream = tokio_tungstenite::accept_hdr_async(raw_stream, |request: &Request, response| {
-        client_header = request.headers().get("sec-websocket-protocol").cloned();
+        client_header = request.headers().get("user_id").cloned();
 
         Ok(response)
     })
@@ -72,26 +77,7 @@ async fn init_tcp_listener() {
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
-    // init_tcp_listener().await;
-
-    let database_url = "mysql://root:root@mysql:3306/rt_chat_db";
-
-    let pool = mysql_async::Pool::new(database_url);
-    let mut conn = pool.get_conn().await?;
-
-    let users = vec![
-        User { user_nick: "Eduardo".to_string() }
-    ];
-
-    conn.exec_drop("INSERT INTO User (user_nick) VALUES (:user_nick)", params! { "user_nick" => users.get(0).unwrap().user_nick.clone() }).await?;
-
-    // "INSERT INTO User (user_nick) VALUES (:user_nick)"
-    //     .que
-        // .with(users.iter().map(|user| params! {
-        //     "user_nick" => user.user_nick.clone()
-        // }))
-        // .batch(&mut conn)
-
+async fn main() -> Result<(), ()> {
+    init_tcp_listener().await;
     Ok(())
 }
